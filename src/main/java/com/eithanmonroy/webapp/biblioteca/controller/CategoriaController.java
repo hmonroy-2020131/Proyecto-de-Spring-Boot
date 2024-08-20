28565757636f643072a560a22bec38501428da38
hmonroy-2020131 <hmonroy-2020131@kinal.edu.gt>
2024-08-20 11:43:42 -0600

    Linea del server

--- src/main/java/com/eithanmonroy/webapp/biblioteca/controller/CategoriaController.java
+++ src/main/java/com/eithanmonroy/webapp/biblioteca/controller/CategoriaController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import com.eithanmonroy.webapp.biblioteca.service.CategoriaService;
 @Controller
 @RestController
 @RequestMapping("")
+@CrossOrigin(value = "http://127.0.0.1:5500")
 
 public class CategoriaController {
 
